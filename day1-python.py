6551227423e50601721fd15063bb53f4784ec20e
deepakmarwal09-art <deepakmarwal09@gmail.com>
2025-11-25 18:53:28 +0530

    Update day1-python.py

--- day1-python.py
+++ day1-python.py
@@ -4,3 +4,14 @@ a, b = 0, 1
 for i in range(n):
     print(a, end=" ")
     a, b = b, a+b
+
+#Reverse a number
+n = int(input())
+rev = int(str(n)[::-1])
+print(rev)
+
+#Print table
+n = int(input())
+for i in range(1, 11):
+    print(n, "x", i, "=", n*i)
+
